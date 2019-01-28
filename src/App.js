807155d899cbd5d8938581e33003b7de957c9366
h_hirata <overdive1225@gmail.com>
2019-01-28 10:52:46 +0900

    FirstCommit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import './main.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+
+  };
+
+  render() {
+    return (
+      <div>
+       <main />
+
+      </div>
+    );
+  }
+
+
+
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './main.css';
+
+class main extends Compornent {
+render() {
+  return (
+    <div className="mainPage">
+     <h1>Plans Book</h1>
+    </div>
+  );
+}
+}
